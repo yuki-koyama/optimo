dc4d9cf46f8a1eefe36bf7ef60dd0ca32eed847b
Yuki Koyama <yuki@koyama.xyz>
2018-07-04 13:55:34 +0900

    Add RBDL v2.5.0 source files

---
+++ external/rbdl-v2.5.0/src/Logging.cc
@@ -0,0 +1,14 @@
+/*
+ * RBDL - Rigid Body Dynamics Library
+ * Copyright (c) 2011-2016 Martin Felis <martin.felis@iwr.uni-heidelberg.de>
+ *
+ * Licensed under the zlib license. See LICENSE for more details.
+ */
+
+#include "rbdl/Logging.h"
+
+RBDL_DLLAPI std::ostringstream LogOutput;
+
+RBDL_DLLAPI void ClearLogOutput() {
+  LogOutput.str("");
+}
